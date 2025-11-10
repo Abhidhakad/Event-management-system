80870bc72d3926da9418872c8e78de617fdfd3c0
Abhishek <nabhishek734@gmail.com>
2025-11-10 21:01:28 +0530

    frontend_import bug fix

--- frontend/src/pages/CreateEvent.jsx
+++ frontend/src/pages/CreateEvent.jsx
@@ -9,7 +9,7 @@ import { Textarea } from '@/components/ui/textarea';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from '@/components/ui/form';
 import { insertEventSchema } from '@/validations/eventValidation';
 import { Calendar,Loader2 } from 'lucide-react';
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from '@/context/authContext';
 import { toast } from 'sonner';
 import { apiFetch } from '@/api/apiClient';
 
